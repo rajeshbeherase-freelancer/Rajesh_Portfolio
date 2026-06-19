5ad1b2d8bc7ce1597ce5fbf8e4770bf515d24c2c
rajeshbeherase-freelancer <rajeshbehera.se@gmail.com>
2026-06-20 02:12:12 +0530

    New Changes In portofolio

--- src/test/java/Baseclasses/Shreekant.java
+++ src/test/java/Baseclasses/Shreekant.java
@@ -1,8 +1,79 @@
 package Baseclasses;
 
+import java.util.Scanner;
+
 public class Shreekant { 
 	public static void main(String[] args) {
-		System.out.println("Chandan");
+		
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter the size of the Array");
+//		int Size=sc.nextInt();
+//		int arr[]=new int[Size];
+//		
+//		for (int i=0; i<arr.length; i++)
+//		{
+//			arr[i]=sc.nextInt();
+//		}
+//		for (int i=0; i<arr.length; i++)
+//		{
+//			System.out.println("Array Element index:- "+i+" is: "+arr[i]);
+//		}
+//		int max=Integer.MIN_VALUE;
+//		int min=Integer.MAX_VALUE;
+//		for (int i=0; i<arr.length; i++)
+//		{
+//			if(arr[i]>=max)
+//			{
+//				max=arr[i];
+//			}
+//			else if(arr[i]<=min)
+//			{
+//				min=arr[i];
+//			}
+//		}
+//		System.out.println("MAX Value:- "+max);
+//		System.out.println("MIN Value:- "+min);
+		
+//		System.out.println("Enter a String");
+//		String S=sc.nextLine();
+//		String[] All_Split=S.split(" ");
+//		for (int i=0; i<All_Split.length; i++)
+//		{
+//			System.out.println("Length of the this word is:- "+All_Split[i]+" "+ All_Split[i].length());
+//		}
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the length of the Array");
+		int Size=sc.nextInt();
+		String arr[]=new String[Size];
+		sc.nextLine();
+		for(int i=0; i<arr.length; i++)
+		{
+			arr[i]=sc.nextLine();
+		}
+		String got=arr[0];
+		String notgot=arr[0];
+		System.out.println("Enter the search elements to find");
+		String Search_Element=sc.nextLine();
+		
+		for(int i=0; i<arr.length; i++)
+		{
+			if(Search_Element.equals(arr[i]))
+			{
+				got=arr[i];
+				break;
+			}
+			else
+			{
+				notgot=arr[i];
+			}
+		}
+		if(Search_Element.equals(got))
+		{
+			System.out.println("The word :- "+Search_Element+" is present");
+		}
+		else
+		{
+			System.out.println("The word :- "+Search_Element+" is not present");
+		}
 	}
-
 }
--- src/test/java/Java_Programms_Practice/Upload_popup.java
+++ src/test/java/Java_Programms_Practice/Upload_popup.java
@@ -1,6 +1,8 @@
 package Java_Programms_Practice;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
@@ -14,7 +16,7 @@ import org.testng.annotations.Test;
 public class Upload_popup {
 	
 	@Test 
-	public void M1() throws InterruptedException, IOException {
+	public void M1() throws InterruptedException, IOException, URISyntaxException {
 		System.out.println("Chandan");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://the-internet.herokuapp.com/upload");
@@ -23,8 +25,14 @@ public class Upload_popup {
 		WebElement Upload=wait.until(ExpectedConditions.elementToBeClickable((By.id("drag-drop-upload"))));
 		Upload.click();
 		Thread.sleep(1000);
-		Runtime.getRuntime().exec("C:\\Users\\chand\\Music\\FileUploadPopup.exe");
 		
+//		This below line is a common way if we are saving the AutoIT script in our Local System 
+//		Runtime.getRuntime().exec("C:\\Users\\chand\\Music\\FileUploadPopup.exe");
+		
+//		This below line is used to fetch the File from the src/test/resource file 
+		String exePath = new File(getClass().getClassLoader().getResource("All_Required_Files/FileUploadPopup.exe").toURI()).getAbsolutePath();
+
+		Runtime.getRuntime().exec(exePath);
 		Thread.sleep(5000);
 		
 		
